116bd0d2cf522b13f2686c219b18b4f45749e8b8
DjayZa <djayza21@gmail.com>
2026-08-27 02:25:18 +0700

    Fix: Patched full-screen text-input focus layer locks via native layer-shell parameter overrides

--- mixxx-kb-toggle.sh
+++ mixxx-kb-toggle.sh
@@ -2,5 +2,7 @@
 if pgrep -x "wvkbd-mobintl" > /dev/null; then
     pkill -x wvkbd-mobintl
 else
-    QT_IM_MODULE=wayland wvkbd-mobintl -H 300 &
+    # -L forces it to use layer-shell to overlay full-screen exclusive apps
+    # -bg sets background transparency blending rules
+    QT_IM_MODULE=wayland wvkbd-mobintl -H 300 -L overlay -bg 000000aa &
 fi
