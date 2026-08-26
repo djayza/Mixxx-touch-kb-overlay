0c147398701879c5cfe08ca9d22ca84fb7bbde9b
DjayZa <djayza21@gmail.com>
2026-08-27 02:39:16 +0700

    Fix: Configured wvkbd to use native layer-shell overlay parameter strings to pop up over full-screen Mixxx

--- mixxx-kb-toggle.sh
+++ mixxx-kb-toggle.sh
@@ -2,7 +2,6 @@
 if pgrep -x "wvkbd-mobintl" > /dev/null; then
     pkill -x wvkbd-mobintl
 else
-    # -L forces it to use layer-shell to overlay full-screen exclusive apps
-    # -bg sets background transparency blending rules
-    QT_IM_MODULE=wayland wvkbd-mobintl -H 300 -L overlay -bg 000000aa &
+    # -L overlay instructs Labwc to forcefully keep wvkbd stacked on top of full-screen viewports
+    QT_IM_MODULE=wayland wvkbd-mobintl -H 300 -L overlay &
 fi
